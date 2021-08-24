c03002900c2c6538b4d5a4e810dce518ad7b7307
gmpravin <gmpravin79@gmail.com>
2021-08-24 20:47:54 +0530

    Added Changes

--- arrays/ctci-array-left-rotation.js
+++ arrays/ctci-array-left-rotation.js
@@ -2,7 +2,7 @@
 function rotLeft(a, d) {
     var left = [];
     var right = [];
-    var n=a.forEach((i,j) => {
+    a.forEach((i,j) => {
         if(j>=d){ left.push(i) }
         if(j<d){ right.push(i) }
     });
